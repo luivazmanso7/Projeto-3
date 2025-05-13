b2cb9363ee30b1522ab76297534d304e02c7b60c
João Vitor Moraes <167870044+Joao-Vitor-Moraes@users.noreply.github.com>
2025-05-13 19:35:26 -0300

    Update ForumRepository.java

--- backend/src/main/java/com/projeto3/backend/repository/ForumRepository.java
+++ backend/src/main/java/com/projeto3/backend/repository/ForumRepository.java
@@ -3,4 +3,9 @@ package com.projeto3.backend.repository;
 import com.projeto3.backend.model.Forum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ForumRepository extends JpaRepository<Forum, Integer> {}
+import java.util.List;
+
+public interface ForumRepository extends JpaRepository<Forum, Integer> {
+
+    List<Forum> findByTituloContainingIgnoreCase(String titulo);
+}
