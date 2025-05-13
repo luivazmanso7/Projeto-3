f42c4450648b08e0c84f8d48188fc512ec183c16
João Vitor Moraes <167870044+Joao-Vitor-Moraes@users.noreply.github.com>
2025-05-13 19:34:53 -0300

    Update CursoRepository.java

--- backend/src/main/java/com/projeto3/backend/repository/CursoRepository.java
+++ backend/src/main/java/com/projeto3/backend/repository/CursoRepository.java
@@ -3,4 +3,11 @@ package com.projeto3.backend.repository;
 import com.projeto3.backend.model.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CursoRepository extends JpaRepository<Curso, Integer> {}
+import java.util.List;
+
+public interface CursoRepository extends JpaRepository<Curso, Integer> {
+
+    List<Curso> findByCursoNomeContainingIgnoreCase(String cursoNome);
+
+    boolean existsByCursoNome(String cursoNome);
+}
