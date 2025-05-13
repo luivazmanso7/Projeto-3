0949584434dfa52a55ecef5cc21a08aaf2764430
João Vitor Moraes <167870044+Joao-Vitor-Moraes@users.noreply.github.com>
2025-05-13 19:35:09 -0300

    Update CertificadoRepository.java

--- backend/src/main/java/com/projeto3/backend/repository/CertificadoRepository.java
+++ backend/src/main/java/com/projeto3/backend/repository/CertificadoRepository.java
@@ -3,4 +3,13 @@ package com.projeto3.backend.repository;
 import com.projeto3.backend.model.Certificado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CertificadoRepository extends JpaRepository<Certificado, Integer> {}
+import java.util.List;
+
+public interface CertificadoRepository extends JpaRepository<Certificado, Integer> {
+
+    List<Certificado> findByNomeUsuario(String nomeUsuario);
+
+    List<Certificado> findByNomeCurso(String nomeCurso);
+
+    List<Certificado> findByNomeUsuarioAndNomeCurso(String nomeUsuario, String nomeCurso);
+}
