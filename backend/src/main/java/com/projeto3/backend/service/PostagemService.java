9f56b098de2ac7fceb6d18c61494dd7f72931a32
João Vitor Moraes <167870044+Joao-Vitor-Moraes@users.noreply.github.com>
2025-05-14 11:25:30 -0300

    Create PostagemService.java

---
+++ backend/src/main/java/com/projeto3/backend/service/PostagemService.java
@@ -0,0 +1,45 @@
+package com.projeto.backend.service;
+
+import com.projeto.backend.entity.Postagem;
+import com.projeto.backend.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostagemService {
+
+    @Autowired
+    private PostagemRepository postagemRepository;
+
+    public Postagem salvarPostagem(Postagem postagem) {
+        return postagemRepository.save(postagem);
+    }
+
+    public List<Postagem> listarPostagens() {
+        return postagemRepository.findAll();
+    }
+
+    public Optional<Postagem> buscarPorId(Long id) {
+        return postagemRepository.findById(id);
+    }
+
+    public List<Postagem> buscarPorConteudo(String conteudo) {
+        return postagemRepository.findByConteudoContainingIgnoreCase(conteudo);
+    }
+
+    public List<Postagem> buscarPorAutorId(Long autorId) {
+        return postagemRepository.findByAutorId(autorId);
+    }
+
+    public List<Postagem> buscarPorData(LocalDate inicio, LocalDate fim) {
+        return postagemRepository.findByDataPostagemBetween(inicio, fim);
+    }
+
+    public void excluirPostagem(Long id) {
+        postagemRepository.deleteById(id);
+    }
+}
