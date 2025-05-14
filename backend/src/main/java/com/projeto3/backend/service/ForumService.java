0e9727436fe5429c5919584047228491392b2eea
João Vitor Moraes <167870044+Joao-Vitor-Moraes@users.noreply.github.com>
2025-05-14 11:26:31 -0300

    Create ForumService.java

---
+++ backend/src/main/java/com/projeto3/backend/service/ForumService.java
@@ -0,0 +1,36 @@
+package com.projeto.backend.service;
+
+import com.projeto.backend.entity.Forum;
+import com.projeto.backend.repository.ForumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ForumService {
+
+    @Autowired
+    private ForumRepository forumRepository;
+
+    public Forum salvarForum(Forum forum) {
+        return forumRepository.save(forum);
+    }
+
+    public List<Forum> listarTodos() {
+        return forumRepository.findAll();
+    }
+
+    public Optional<Forum> buscarPorId(Long id) {
+        return forumRepository.findById(id);
+    }
+
+    public List<Forum> buscarPorTitulo(String titulo) {
+        return forumRepository.findByTituloContainingIgnoreCase(titulo);
+    }
+
+    public void excluirForum(Long id) {
+        forumRepository.deleteById(id);
+    }
+}
